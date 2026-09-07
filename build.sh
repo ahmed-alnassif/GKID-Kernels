344af49a50e0ad2cd53a8dac32a117c434bb2f9a
Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>
2026-09-07 18:29:07 +0300

    fix: use fix_task_mmu_corruption only for 6.12
    Signed-off-by: Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>

--- build.sh
+++ build.sh
@@ -279,7 +279,9 @@ fi
 echo "::endgroup::"
 set +eo pipefail
 
-fix_task_mmu_corruption
+if [ "$KERNEL_VERSION" = "6.12" ]; then
+  fix_task_mmu_corruption
+fi
 
 AK3_ZIP_NAME=${AK3_ZIP_NAME//KVER/$LINUX_VERSION}
 AK3_ZIP_NAME=${AK3_ZIP_NAME//VARIANT/$VARIANT}
