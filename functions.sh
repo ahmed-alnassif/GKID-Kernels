9d1c5654f881ca894a8d1a28268b48dbcde0ccd3
Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>
2026-09-07 19:16:36 +0300

    fix: disable fuzz on 6.12 SUSFS patches to detect failures
    Signed-off-by: Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>

--- functions.sh
+++ functions.sh
@@ -358,9 +358,15 @@ apply_susfs_patches() {
       cd $OLDPWD
     fi
 
-    if ! patch -p1 --fuzz=3 < "$SUSFS_PATCHES/50_add_susfs_in_${SUSFS_PATCH}.patch"; then
-        if [ "$KERNEL_KMI" = "android13-5.15" ]; then
-            fix_namespace_susfs_mount
+    if [ "$KERNEL_VERSION" = "6.12" ]; then
+        if ! patch -p1 < "$SUSFS_PATCHES/50_add_susfs_in_${SUSFS_PATCH}.patch"; then
+            warning "SUSFS patch failed on 6.12, check .rej files"
+        fi
+    else
+        if ! patch -p1 --fuzz=3 < "$SUSFS_PATCHES/50_add_susfs_in_${SUSFS_PATCH}.patch"; then
+            if [ "$KERNEL_KMI" = "android13-5.15" ]; then
+                fix_namespace_susfs_mount
+            fi
         fi
     fi
 
